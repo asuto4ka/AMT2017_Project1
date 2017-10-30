7e9ee93b3e0b3be3e73d7b33543f65bc19a7973e
nums <bandido18cls15colombiano@hotmail.com>
2017-10-30 20:28:12 +0100

    oublie image Docker

---
+++ images/mysql/Data/b_data.sql
@@ -0,0 +1,7 @@
+USE amtproject;
+
+INSERT INTO books VALUES (1,'ACADEMY DINOSAUR','A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies','author',2006,10),
+(2,'ACE GOLDFINGER','A Astounding Epistle of a Database Administrator And a Explorer who must Find a Car in Ancient China','author',2006,10),
+(3,'ADAPTATION HOLES','A Astounding Reflection of a Lumberjack And a Car who must Sink a Lumberjack in A Baloon', 'author',2006,10);
+
+INSERT INTO users VALUES (1,'admin','[13, -74, 97, -19, -74, -120, 15, 79, -116, 9, 34, 107, -39, -71, -95, -88, -93, -119, 78, -85, 98, -57, -23, 26, 5, -127, 31, -62, -100, 40, -111, -108]');
